792a95c471134ae38464a45bd133a1d673fe0f68
Saymon Tavares <saymon.tavares@satc.edu.br>
2021-12-17 17:53:01 -0300

    List Favorite Movies

--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -4,8 +4,14 @@
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-12">
                     <div class="row justify-content-between align-items-center">
-                        <div class="col-12">
-                        <h4 class="mb-3 mb-md-0">Filmes <b>LANÇAMENTOS</b></h4>
+                        <div class="col-10">
+                            <h4 class="mb-3 mb-md-0">Filmes <b>LANÇAMENTOS</b></h4>
+                        </div>
+
+                        <div class="col-2">
+                            <button type="button" class="btn btn-primary btn-block">
+                                <i class="bi bi-heart-fill mr-1" style="color: #e35d6a"></i>Meus Favoritos
+                            </button>
                         </div>
                     </div>
                 </div>
